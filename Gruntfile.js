9eea1442c432898e5c0b887e858cd7e26a364843
ProjectSoft <projectsoft2009@yandex.ru>
2025-03-01 22:25:08 +0400

    update

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+	var fs = require('fs'),
+		path = require('path'),
+		chalk = require('chalk'),
+		hash = function (...args) {
+			var md5 = require('md5');
+			let result = "",
+				arr = [];
+			if(!args.length){
+				let time = (new Date()).getTime();
+				arr.push("Not arguments");
+				result = md5(time).toString();
+			}else{
+				let text = "";
+				for(let index in args){
+					let file = args[index];
+					file = path.normalize(path.join(__dirname, file));
+					try{
+						let buff = fs.readFileSync(file, {
+							encoding: "utf8"
+						}).toString();
+						text += buff;
+						arr.push(file);
+					}catch(e){
+						// Ничего не делаем
+						arr.push("Not found");
+					}
+				}
+				result = md5(text).toString();
+			}
+			arr.push(result);
+			grunt.log.oklns([chalk.cyan("Generate hash:") + "\n" + chalk.yellow(arr.join("\n"))]);
+			return result;
+		};
+}
